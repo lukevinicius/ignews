a41fcbc09a1b12b9451971f0a94db7c4b541d367
lucas vinicius <lucasviniciusaalves@gmail.com>
2022-02-25 11:32:31 -0300

    add: faunadb and Stripe

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,18 +1,24 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession } from 'next-auth/client';
+
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLogged = false;
+  const [session] = useSession();
 
-  return isUserLogged ? (
+  return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
       Lucas vinicius
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sign In with GitHub
     </button>
